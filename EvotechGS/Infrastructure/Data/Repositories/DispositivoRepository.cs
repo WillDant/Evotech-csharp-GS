c7ffbdaf08d7ee1d76c4e1549b5d0253b268f9e0
willdant <danielwillian532@gmail.com>
2024-11-17 21:00:30 -0300

    Construção dos Repositories concretos e ajustes no Context

--- EvotechGS/Infrastructure/Data/Repositories/DispositivoRepository.cs
+++ EvotechGS/Infrastructure/Data/Repositories/DispositivoRepository.cs
@@ -1,31 +1,32 @@
 ﻿using EvotechGS.Domain.Entities;
 using EvotechGS.Domain.Interfaces;
+using EvotechGS.Infrastructure.Data.AppData;
 
 namespace EvotechGS.Infrastructure.Data.Repositories
 {
     public class DispositivoRepository : IDispositivoRepository
     {
-        private readonly Contexto _contexto;
-        public DispositivoRepository(Contexto contexto)
+        private readonly ApplicationContext _contexto;
+        public DispositivoRepository(ApplicationContext contexto)
         {
             _contexto = contexto;
         }
         // Listar todos os dispositivos
         public List<DispositivoEntity> ListarDispositivos()
         {
-            return _contexto.Dispositivos.ToList();
+            return _contexto.Dispositivo.ToList();
         }
         // Listar um dispositivo
         public DispositivoEntity? ObterDispositivoPorId(int id)
         {
-            var dispositivo = _contexto.Dispositivos.FirstOrDefault(x => x.Id == id);
+            var dispositivo = _contexto.Dispositivo.FirstOrDefault(x => x.id_dispositivo == id);
             return dispositivo;
         }
 
         // Salvar um dispositivo
         public DispositivoEntity? SalvarDispositivo(DispositivoEntity model)
         {
-            _contexto.Dispositivos.Add(model);
+            _contexto.Dispositivo.Add(model);
             _contexto.SaveChanges();
             return model;
         }
@@ -34,8 +35,8 @@ namespace EvotechGS.Infrastructure.Data.Repositories
         public DispositivoEntity? AtualizarDispositivo(int id, DispositivoEntity model)
         {
             var dispositivo = _contexto
-            .Dispositivos
-            .FirstOrDefault(x => x.Id == id);
+            .Dispositivo
+            .FirstOrDefault(x => x.id_dispositivo == id);
 
             if (dispositivo is not null)
             {
@@ -50,11 +51,11 @@ namespace EvotechGS.Infrastructure.Data.Repositories
         public DispositivoEntity? DeletarDispositivo(int id)
         {
             var dispositivo = _contexto
-            .Dispositivos
-            .FirstOrDefault(x => x.Id == id);
+            .Dispositivo
+            .FirstOrDefault(x => x.id_dispositivo == id);
             if (dispositivo is not null)
             {
-                _contexto.Dispositivos.Remove(dispositivo);
+                _contexto.Dispositivo.Remove(dispositivo);
                 _contexto.SaveChanges();
                 return dispositivo;
             }
--- EvotechGS/Infrastructure/Data/Repositories/UsuarioRepository.cs
+++ EvotechGS/Infrastructure/Data/Repositories/UsuarioRepository.cs
@@ -1,6 +1,73 @@
-﻿namespace EvotechGS.Infrastructure.Data.Repositories
+﻿using EvotechGS.Domain.Entities;
+using EvotechGS.Domain.Interfaces;
+using EvotechGS.Infrastructure.Data.AppData;
+
+namespace EvotechGS.Infrastructure.Data.Repositories
 {
-    public class UsuarioRepository
+    public class UsuarioRepository : IUsuarioRepository
     {
+        private readonly ApplicationContext _context;
+
+        public UsuarioRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        // Listar todos os usuários
+        public List<UsuarioEntity> ListarUsuarios()
+        {
+            return _context.Usuario.ToList();
+        }
+
+        // Listar um usuário
+        public UsuarioEntity? ObterUsuarioPorId(int id)
+        {
+            var usuario = _context.Usuario.FirstOrDefault(x => x.id_usuario == id);
+            return usuario;
+        }
+
+        // Salvar um usuário
+        public UsuarioEntity? SalvarUsuario(UsuarioEntity model)
+        {
+            _context.Usuario.Add(model);
+            _context.SaveChanges();
+            return model;
+        }
+
+        // Atualizar um usuário
+        public UsuarioEntity? AtualizarUsuario(int id, UsuarioEntity model)
+        {
+            var usuario = _context
+            .Usuario
+            .FirstOrDefault(x => x.id_usuario == id);
+
+            if (usuario is not null)
+            {
+                usuario.nm_usuario = model.nm_usuario;
+                usuario.email = model.email;
+                usuario.senha = model.senha;
+                usuario.telefone = model.telefone;
+
+                _context.SaveChanges();
+                return usuario;
+            }
+            return model;
+        }
+
+        // Deletar um usuário
+        public UsuarioEntity? DeletarUsuario(int id)
+        {
+            var usuario = _context
+            .Usuario
+            .FirstOrDefault(x => x.id_usuario == id);
+
+            if (usuario is not null)
+            {
+                _context.Usuario.Remove(usuario);
+                _context.SaveChanges();
+                return usuario;
+            }
+            return new UsuarioEntity();
+        }
     }
 }
