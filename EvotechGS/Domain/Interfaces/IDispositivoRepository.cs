049334be63424f2302387f73d62f4d68dbfe0f8b
willdant <danielwillian532@gmail.com>
2024-11-17 21:29:22 -0300

    Implementando as Interfaces dos Repos

--- EvotechGS/Domain/Interfaces/IDispositivoRepository.cs
+++ EvotechGS/Domain/Interfaces/IDispositivoRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace EvotechGS.Domain.Interfaces
+﻿using EvotechGS.Domain.Entities;
+
+namespace EvotechGS.Domain.Interfaces
 {
-    public class IDispositivoRepository
+    public interface IDispositivoRepository
     {
+        IEnumerable<DispositivoEntity> ObterTodosDispositivos();
+        DispositivoEntity? ObterDispositivoPorId(int id);
+        DispositivoEntity? SalvarDispositivo(DispositivoEntity entity);
+        DispositivoEntity? AtualizarDispositivo(DispositivoEntity entity);
+        DispositivoEntity? DeletarDispositivo(int id);
     }
 }
--- EvotechGS/Domain/Interfaces/IUsuarioRepository.cs
+++ EvotechGS/Domain/Interfaces/IUsuarioRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace EvotechGS.Domain.Interfaces
+﻿using EvotechGS.Domain.Entities;
+
+
+namespace EvotechGS.Domain.Interfaces
 {
-    public class IUsuarioRepository
+    public interface IUsuarioRepository
     {
+        IEnumerable<UsuarioEntity> ObterTodosUsuarios();
+        UsuarioEntity? ObterUsuarioPorId(int id);
+        UsuarioEntity? SalvarUsuario(UsuarioEntity entity);
+        UsuarioEntity? AtualizarUsuario(UsuarioEntity entity);
+        UsuarioEntity? DeletarUsuario(int id);
     }
 }
